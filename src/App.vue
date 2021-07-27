3ccb601cc2e8b4c57963219d45e3ef6778d65a05
Thai Morris <thaiimanimorris@live.com>
2021-07-27 19:18:57 -0400

    this actually looks good now

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/icons/target.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <body>
+     <div>
+
+  <img alt="Lakfu" src="./assets/icons/target.png">
+  <h1> Welcome to Lakfu </h1>
+  <button class="button">I'm an entrepreneur</button>
+  <br>
+  <br>
+  <button class="button">I'm an investor</button>
+  </div>
+  </body>
+ 
+
+
+
   
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style>
+body{
+  background-image: url('./assets/detroit.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center; 
+}
+
+.button {
+  color: whitesmoke;
+  font-weight: bolder;
+    box-shadow: 5px 10px 18px #888888;
+    background: orange;
+    border-radius: 25px;
+    padding: 1%;
+    border: none;
+}
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 33%;
+  background: none;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
